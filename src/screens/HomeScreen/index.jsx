97fa6408f98aba217e7a77ebc41914740550b713
Vitor Brancalião <vitor.brancaliao@outlook.com>
2023-05-01 19:25:05 -0300

    first commit

---
+++ src/screens/HomeScreen/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <title>The Woods</title>
+      </Head>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
